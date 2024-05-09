8d7eb29a8081c3dd63a05e07ffbf0c112589d682
Helkayam <henelkayam99@gmail.com>
2024-05-09 20:18:46 +0300

    I implemented the Ray and Vector classes

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -1,4 +1,37 @@
 package primitives;
-
+/**
+ * Class Ray is the basic class representing a fundamental object in 3D geometry, the group of points on a straight line that are on one side of a given point
+ * called the head of the beam, defined by fields of point and direction
+ */
 public class Ray {
+    private final Point head;
+    private final Vector direction;
+
+    /**
+     * Constructs a new ray with the given head and direction.
+     *
+     * @param head      The starting point of the ray.
+     * @param direction The direction vector of the ray.
+     */
+    public Ray(Point head, Vector direction) {
+        this.head = head;
+        // Normalize the direction vector
+        this.direction = direction.normalize();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        return (obj instanceof Ray other)
+                && this.head.equals(other.head)
+                && this.direction.equals(other.direction);
+    }
+
+    @Override
+    public String toString() {
+        return "Ray{" +
+                "head=" + head +
+                ", direction=" + direction +
+                '}';
+    }
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,4 +1,116 @@
 package primitives;
 
+/**
+ *The class represents a vector which is a fundamental object in 3D geometry, with direction and size,
+ * The class inherits from the Point class
+ */
 public class Vector extends Point {
+
+    /**
+     * Constructs a new vector with the given coordinates.
+     *
+     * @param x The x-coordinate.
+     * @param y The y-coordinate.
+     * @param z The z-coordinate.
+     * @throws IllegalArgumentException if the zero vector is inserted.
+     */
+    public Vector(double x, double y, double z) {
+        if (x == 0 && y == 0 && z == 0)
+            throw new IllegalArgumentException("The zero vector must not be inserted");
+        super(x, y, z);
+    }
+
+    /**
+     * Constructs a new vector from a Double3 object.
+     *
+     * @param obj The Double3 object.
+     * @throws IllegalArgumentException if the zero vector is inserted.
+     */
+    public Vector(Double3 obj) {
+        if (obj.equals(Double3.ZERO))
+            throw new IllegalArgumentException("The zero vector must not be inserted");
+        super(obj);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        return (obj instanceof Vector other)
+                && super.equals(other);
+    }
+
+    @Override
+    public String toString() {
+        return (super.toString());
+    }
+
+    /**
+     *This method adds another vector to this vector.
+     *
+     * @param vecToAdd The vector to add.
+     * @return The resulting vector.
+     */
+    public Vector add(Vector vecToAdd) {
+        return new Vector(super.add(vecToAdd));
+    }
+
+    /**
+     *This method scales this vector by a scalar.
+     *
+     * @param num The scalar to scale by.
+     * @return The scaled vector.
+     */
+    public Vector scale(double num) {
+        return new Vector(xyz.scale(num));
+    }
+
+    /**
+     * This method calculates the dot product of this vector and another vector.
+     *
+     * @param vec The other vector.
+     * @return The dot product.
+     */
+    public double dotProduct(Vector vec) {
+        return (xyz.d1 * vec.xyz.d1 + xyz.d2 * vec.xyz.d2 + xyz.d3 * vec.xyz.d3);
+    }
+
+    /**
+     * This method calculates the cross product of this vector and another vector.
+     *
+     * @param vec The other vector.
+     * @return The cross product vector.
+     */
+    public Vector crossProduct(Vector vec) {
+        return new Vector(xyz.d2 * vec.xyz.d3 - xyz.d3 * vec.xyz.d2,
+                xyz.d3 * vec.xyz.d1 - xyz.d1 * vec.xyz.d3,
+                xyz.d1 * vec.xyz.d2 - xyz.d2 * vec.xyz.d1);
+    }
+
+    /**
+     * This method calculates the squared length of this vector.
+     *
+     * @return The squared length.
+     */
+    public double lengthSquared() {
+        return super.distanceSquared(new Point(0d, 0d, 0d));
+    }
+
+    /**
+     * This method calculates the length of this vector.
+     *
+     * @return The length.
+     */
+    public double length() {
+        return Math.sqrt(lengthSquared());
+    }
+
+    /**
+     * This method normalizes this vector.
+     *
+     * @return The normalized vector.
+     */
+    public Vector normalize() {
+        return scale(1 / length());
+    }
+
 }
