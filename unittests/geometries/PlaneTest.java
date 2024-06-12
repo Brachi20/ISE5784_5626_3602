99554f10df0ebd6cdd3a632426f12dcc4369acfc
Brachi20 <b0583919569@gmail.com>
2024-06-12 22:30:22 +0300

    fix the testFindIntersections of plane

--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -8,11 +8,11 @@ import primitives.Vector;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlaneTest {
-    Point p1=new Point(0d,5d,0d);
-    Point p2=new Point(1d,0d,0d);
-    Point p3=new Point(-1d,10d,0d);
-    Point p4=new Point(1d,2d,0d);
-    Plane p=new Plane(p1,p2,p4);
+    Point p050 =new Point(0d,5d,0d);
+    Point p100 =new Point(1d,0d,0d);
+    Point p1 =new Point(-1d,10d,0d);
+    Point p120 =new Point(1d,2d,0d);
+    Plane p=new Plane(p050, p100, p120);
 
     @Test
     public void testConstructor(){
@@ -25,10 +25,10 @@ class PlaneTest {
         // =============== Boundary Values Tests ==================
 
         // TC11: A test for building a plan with coalesce points
-        assertThrows(IllegalArgumentException.class,()->new Plane(p1, p1, p2),
+        assertThrows(IllegalArgumentException.class,()->new Plane(p050, p050, p100),
                 "ERROR:coalesce point in plane, did not throw an exception");
         // TC12: A test for building a plan with 3 point on the same line
-        assertThrows(IllegalArgumentException.class,()->new Plane(p1,p2,p3),
+        assertThrows(IllegalArgumentException.class,()->new Plane(p050, p100, p1),
                 "ERROR: the points are on the same line");
     }
 
@@ -47,61 +47,53 @@ class PlaneTest {
         Vector v=new Vector(0,0,1);
         Vector v1=new Vector(1,1,2);
         Ray r1 =new Ray(new Point(0,0,-1),v1);
-        Ray r2=new Ray(new Point(1,1,1),new Vector(1,1,1));
-        Ray r3=new Ray(p1,new Vector(1d,1d,0d));
-        Ray r4=new Ray(new Point(0,5,1),new Vector(0,-5,1));
-        Ray r5=new Ray(p2,v);
+        Ray r2=new Ray(new Point(1,1,1),v1);
+        Ray r3=new Ray(p050,new Vector(1d,1d,0d));
+        Ray r4=new Ray(new Point(0,5,1),new Vector(0,-10,0));
+        Ray r5=new Ray(p100,v);
         Ray r6=new Ray(new Point(1,0,-1),v);
         Ray r7=new Ray(new Point(1,0,1),v);
-        Plane p2=new Plane(p4,v);
-        Ray r8 =new Ray(p4,v1);
-        Ray r9 =new Ray(p1,v1);
+        Ray r8 =new Ray(p120,v1);
+        Ray r9 =new Ray(p050,v1);
+        Plane p2=new Plane(p120,v);
 
         // ============ Equivalence Partitions Tests ==============
 
-        // TC01: ray does not vertical ant not parallel to the plane
-        assertEquals(1,p2.findIntersections(r1).size(),"calculate intersections between a plane and a" +
-                " ray that is not parallel and not vertical do not return one intersection point");
-
-        // TC02: ray does not vertical ant not parallel to the plane without an intersection point
+        // TC01: ray does not vertical ant not parallel to the plane (1 point)
+        assertEquals(1,p2.findIntersections(r1).size(),"Wrong number of points");
+        assertEquals(new Point(0.5,0.5,0),p2.findIntersections(r1).getFirst(),"Ray not vertical and not parallel to plane - Wrong point");
+        // TC02: ray does not vertical ant not parallel after the plane to the plane (0 point)
         assertNull(p2.findIntersections(r2),"calculate intersections between a plane and a" +
                 " ray that is not parallel and not vertical should not have to return intersection point ");
 
         // =============== Boundary Values Tests ==================
 
-        // TC11: calculate intersection points between plane and ray that parallel and on the plane
-        assertNull(p2.findIntersections(r3),"calculate intersections between a plane and " +
-                "a ray that parallel and on the plane should not have to return intersection point ");
+        // TC11: calculate intersection points between plane and ray that parallel and on the plane (0 point)
+        assertNull(p2.findIntersections(r3),"Ray is parallel to the plane and on the plane ");
 
         //TC12:calculate intersection points between plane and ray that parallel and not on the plane
-        assertNull(p2.findIntersections(r4),"calculate intersections between a plane and " +
-                "a ray that parallel and not on the plane should not have to return intersection point ");
+        assertNull(p2.findIntersections(r4),"Ray is parallel to the plane and not on the plane ");
 
-        //TC13:Calculate intersection points between plane and ray that parallel to the plane and start on it
+        //TC13:Calculate intersection points between plane and ray that vertical to the plane and start on it (0 point)
         assertNull(p2.findIntersections(r5),
-                "calculate intersections between a plane and a ray that parallel to the plane and start on it " +
-                        "should not have to return intersection point");
+                " Ray is vertical to the plane and start on it");
 
-        //TC14:Calculate intersection points between plane and ray that vertical to the plane and before the plane
-        assertEquals(1,p2.findIntersections(r6).size(),
-                "calculate intersections between a plane and a ray that parallel to the plane and start before it " +
-                        "should return one intersection point");
+        //TC14:Calculate intersection points between plane and ray that vertical to the plane and before the plane (1 point)
+        assertEquals(1,p2.findIntersections(r6).size(), " Worn number of points");
+        assertEquals(new Point(1,0,0),p2.findIntersections(r6).getFirst(),"Ray is vertical to the plane and before the plane");
 
-        //TC15:Calculate intersection points between plane and ray that parallel to the plane and start after it
+        //TC15:Calculate intersection points between plane and ray that vertical to the plane and start after it (0 point)
         assertNull(p2.findIntersections(r7),
-                "calculate intersections between a plane and a ray that parallel to the plane and start after it " +
-                        "should not have to return intersection point");
+                " Ray is vertical to the plane and start after the plane");
 
-        //TC16:Calculate intersection points between plane and ray that does not vertical ant not parallel to the plane
+        //TC16:Calculate intersection points between plane and ray that do not vertical ant not parallel to the plane (0 point)
         // and start on point which the plane represent by it
         assertNull(p2.findIntersections(r8),
-                "Calculating a cut with a non-perpendicular and non-parallel ray starting at the " +
-                        "representation point should not have returned points");
+                " Ray is not vertical and not parallel to the plane and start on point which the plane represent by it");
 
         //TC17:Calculate intersection points between plane and ray that does not vertical ant not parallel to the plane
-        //and start on the plane
+        //and start on the plane (0 point)
         assertNull(p2.findIntersections(r9),
-                "Calculating a cut with a non-perpendicular and non-parallel ray starting at the " +
-                        "plane should not have returned points");
+                " Ray is not vertical and not parallel to the plane and start on the plane");
     }
 }
