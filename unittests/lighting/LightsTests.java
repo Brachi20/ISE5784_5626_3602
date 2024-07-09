9f52fe90797f81be9f760ccf78cef9ae693864ab
Helkayam <henelkayam99@gmail.com>
2024-07-09 14:40:59 +0300

    fixes

--- unittests/lighting/LightsTests.java
+++ unittests/lighting/LightsTests.java
@@ -79,18 +79,28 @@ public class LightsTests {
      */
     private final Color trianglesLightColor = new Color(800, 500, 250);
     //Lights color for the Multiple light sources in tests with triangles
+    private final Color triangleDirectionalLightColor = new Color(500, 300, 100);
+    private final Color triangleSpotLightColor = new Color(300, 500, 200);
+    private final Color trianglePointLightColor = new Color(200, 200, 500);
 
-   private final Color trianglesLightColor1 = new Color(600, 400, 200);
-
-   /**
+    /**
      * Light color for tests with sphere
      */
     private final Color sphereLightColor = new Color(800, 500, 0);
+    //Lights color for the Multiple light sources in tests with sphere
     /**
      * Light color for tests with Multiple light sources on sphere
      */
-   private final Color sphereLightColor1 = new Color(600, 400, 200);
-   /**
+    private final Color sphereDirectionalLightColor1 = new Color(600, 400, 200);
+    private final Color sphereSpotLightColor1 = new Color(400, 600, 300);
+    private final Color spherePointLightColor1 = new Color(300, 300, 600);
+    // Colors for the Multiple light sources in tests with sphere
+    private final Color sphereDirectionalLightColor = new Color(800, 500, 250);
+    private final Color spherePointLightColor = new Color(800, 500, 0);
+    private final Color sphereSpotLightColor = new Color(500, 250, 250);
+
+
+    /**
      * Color of the sphere
      */
     private final Color sphereColor = new Color(BLUE).reduce(2);
@@ -117,18 +127,29 @@ public class LightsTests {
      * Position of the light in tests with sphere
      */
     private final Point sphereLightPosition = new Point(-50, -50, 25);
-   /**
-        * Position of the Multiple light sources in tests with sphere
+    //Positions for the Multiple light sources in tests with sphere
+    /**
+     * Position of the spotLight sources in tests with sphere
      */
-   private final Point sphereLightPosition1 = new Point(30, 30, 50);
+    private final Point sphereSpotLightPosition = new Point(30, 30, 50);
+    /**
+     * Position of the PointLight sources in tests with sphere
+     */
+    private final Point spherePointLightPosition = new Point(-30, -30, 50);
     /**
      * Light direction (directional and spot) in tests with sphere
      */
     private final Vector sphereLightDirection = new Vector(1, 1, -0.5);
-/**
-     * Light direction (directional and spot) in tests with Multiple light sources on sphere
+    //Directions for the Multiple light sources in tests with sphere
+    /**
+     * DirectionLight direction in tests with Multiple light sources on sphere
      */
-   private final Vector sphereLightDirection1 = new Vector(-1, -1, 0.5);
+    private final Vector sphereDirectionLightDirection = new Vector(-1, -1, 0.5);
+    /**
+     * spotLight direction in tests with Multiple light sources on triangle
+     */
+    private final Vector sphereSpotLightDirection = new Vector(-1, -1, 0.5);
+
 
     /**
      * Position of the DirectionalLight in tests with triangles
@@ -136,29 +157,25 @@ public class LightsTests {
     private final Point trianglesLightPosition = new Point(30, 10, -100);
 
     //Positions for the Multiple light sources in tests with triangles
-    /**
-     * Position of the DirectionalLight in tests with Multiple light sources on triangle
-     */
-    private final Point trianglesLightPosition1 = new Point(30, 10, -100);
 
     /**
      * Position of the spotLight in tests with Multiple light sources on triangle
      */
-    private final Point trianglesLightPosition2 = new Point(-30, 50, -50);
+    private final Point trianglesLightPosition1 = new Point(-30, 50, -50);
     /**
      * Position of the PointLight in tests with Multiple light sources on triangle
-      */
-    private final Point trianglesLightPosition3 = new Point(0, -10, 0);
+     */
+    private final Point trianglesLightPosition2 = new Point(0, -10, 0);
 
     /**
      * Direction of the DirectionalLight in tests with triangles
      */
     private final Vector trianglesLightDirection = new Vector(-2, -2, -2);
     //lights direction for the Multiple light sources in tests with triangles
-   /**
+    /**
      * DirectionLight direction in tests with Multiple light sources on triangle
      */
-   private final Vector trianglesLightDirection1 = new Vector(2, 2, -1);
+    private final Vector trianglesLightDirection1 = new Vector(2, 2, -1);
     /**
      * spotLight direction in tests with Multiple light sources on triangle
      */
@@ -270,25 +287,27 @@ public class LightsTests {
     }
 
     @Test
-   public void MultipleLightSourcesSphere(){
-       scene1.geometries.add(sphere);
-       scene1.lights.add(new DirectionalLight(sphereLightColor1, sphereLightDirection1));
-       scene1.lights.add(new PointLight(sphereLightColor1, sphereLightPosition1).setKl(0.001).setKq(0.0002));
-       scene1.lights.add(new SpotLight(sphereLightColor1, sphereLightPosition1, sphereLightDirection1).setKl(0.001).setKq(0.0001));
+    public void MultipleLightSourcesSphere() {
+        scene1.geometries.add(sphere);
+        scene1.lights.add(new DirectionalLight(sphereDirectionalLightColor, sphereDirectionLightDirection));
+        scene1.lights.add(new PointLight(spherePointLightColor, spherePointLightPosition).setKl(0.001).setKq(0.0002));
+        scene1.lights.add(new SpotLight(sphereSpotLightColor, sphereSpotLightPosition, sphereSpotLightDirection).setKl(0.001).setKq(0.0001));
 
-       camera1.setImageWriter(new ImageWriter("multiLightSphere", 500, 500))
-               .build()
-               .renderImage()
-               .writeToImage();
+        camera1.setImageWriter(new ImageWriter("multiLightSphere", 500, 500))
+                .build()
+                .renderImage()
+                .writeToImage();
 
     }
 
     @Test
-    public void MultipleLightSourcesTriangles(){
+    public void MultipleLightSourcesTriangles() {
         scene2.geometries.add(triangle1, triangle2);
-        scene2.lights.add(new DirectionalLight(trianglesLightColor, trianglesLightDirection1));
-        scene2.lights.add(new PointLight(trianglesLightColor1, trianglesLightPosition2).setKl(0.001).setKq(0.0002));
-        scene2.lights.add(new SpotLight(trianglesLightColor1, trianglesLightPosition3, trianglesLightDirection2).setKl(0.001).setKq(0.0001));
+        scene2.lights.add(new DirectionalLight(triangleDirectionalLightColor, trianglesLightDirection1));
+        scene2.lights.add(new PointLight(trianglePointLightColor, trianglesLightPosition1).setKl(0.0005).setKq(0.003));
+        scene2.lights.add(new SpotLight(triangleSpotLightColor, trianglesLightPosition2, trianglesLightDirection2)
+                .setKl(0.001).setKq(0.0001));
+
 
         camera2.setImageWriter(new ImageWriter("multiLightTriangles", 500, 500))
                 .build()
